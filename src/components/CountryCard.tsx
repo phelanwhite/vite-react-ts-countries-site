9b62cbc479dc93acbbd8424d8f4057fa17741c1c
PhelanWhite <phelanwhite.business@gmail.com>
2024-12-21 13:09:52 +0700

    first commit

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,30 @@
+import { CountryCardType } from "@/types";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CountryCard = ({ data }: { data: CountryCardType }) => {
+  return (
+    <Link to={`/${data.cca2}`}>
+      <div className="shadow rounded overflow-hidden bg-[--element-color]">
+        <div className="aspect-video">
+          <img src={data.flags.png} alt={data.flags.alt} loading="lazy" />
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-bold mb-3">{data.name.common}</h2>
+          <p>
+            <span className="font-semibold">Population:</span> {data.population}
+          </p>
+          <p>
+            <span className="font-semibold">Region:</span> {data.region}
+          </p>
+          <p>
+            <span className="font-semibold">Capital:</span>{" "}
+            {data.capital?.join(", ")}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CountryCard;
